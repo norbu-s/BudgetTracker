4247e07248c56dcf847ea24752c0fb8f6ad26956
Norbu Sherpa <norbuts@hotmail.com>
2021-04-16 11:37:13 +1000

    deploying

--- server.js
+++ server.js
@@ -32,6 +32,4 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-});
+app.listen(process.env.PORT || 5000)
