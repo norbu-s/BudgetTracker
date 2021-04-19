3c3a9621498ecfd3f2672f4d78a312bdf4a24633
Norbu Sherpa <norbuts@hotmail.com>
2021-04-19 22:19:44 +1000

    Updated service worker and index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,17 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
     './',
-    '/manifest.webmanifest',
     './index.html',
     './index.js',
     './db.js',
     './styles.css',
     './icons/icon-192x192.png',
     './icons/icon-512x512.png'
-  ],
+  ];
   
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
+
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
